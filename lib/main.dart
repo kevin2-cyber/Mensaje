77024228a2b474105280c71702c5ba63fe98acb1
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-27 07:43:50 +0000

    name

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
-import '../data/message_dao.dart';
 import '../data/user_dao.dart';
 import 'app/ui/message_list.dart';
 import 'app/ui/login.dart';
@@ -23,6 +22,7 @@ class App extends StatelessWidget {
     // Add MultiProvider
     return MultiProvider(
       providers: [
+
         // Add ChangeNotifierProvider<UserDao> here
         ChangeNotifierProvider<UserDao>(
           lazy: false,
@@ -33,6 +33,7 @@ class App extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Mensaje',
         theme: ThemeData(primaryColor: const Color(0xFF3D814A)),
+
         // Add Consumer<UserDao> here
         home: Consumer<UserDao>(
           builder: (context, userDao, child) {
