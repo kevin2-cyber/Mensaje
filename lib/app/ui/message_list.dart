c0b5d137b557718b16ffda2ebc389729001e044e
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-27 02:20:44 +0000

    minor update

--- lib/app/ui/message_list.dart
+++ lib/app/ui/message_list.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'message_widget.dart';
 import '../../data/message.dart';
 import '/data/message_dao.dart';
-
 
 class MessageList extends StatefulWidget {
   const MessageList({Key? key}) : super(key: key);
@@ -29,8 +30,9 @@ class MessageListState extends State<MessageList> {
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.transparent.withOpacity(0.09),
-        title: Center(child: Text(
-            'Mensaje',
+        title: Center(
+            child: Text(
+          'Mensaje',
           style: Theme.of(context).textTheme.headline5,
         )),
         // TODO: Replace with actions
@@ -39,8 +41,8 @@ class MessageListState extends State<MessageList> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            // TODO: Add Message DAO to _getMessageList
-            _getMessageList(),
+            // Add Message DAO to _getMessageList
+            _getMessageList(messageDao),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -64,7 +66,7 @@ class MessageListState extends State<MessageList> {
                         ? CupertinoIcons.arrow_right_circle_fill
                         : CupertinoIcons.arrow_right_circle),
                     onPressed: () {
-                      // TODO: Add Message DAO 2
+                      // Add Message DAO 2
                       _sendMessage(messageDao);
                     })
               ],
@@ -77,7 +79,7 @@ class MessageListState extends State<MessageList> {
 
   // Replace _sendMessage
   void _sendMessage(MessageDao messageDao) {
-    if(_canSendMessage()) {
+    if (_canSendMessage()) {
       final message = Message(
         text: _messageController.text,
         date: DateTime.now(),
@@ -85,20 +87,40 @@ class MessageListState extends State<MessageList> {
       );
       messageDao.saveMessage(message);
       _messageController.clear();
-      setState(() {
-
-      });
+      setState(() {});
     }
   }
 
-  // TODO: Replace _getMessageList
-  Widget _getMessageList() {
-    return const SizedBox.shrink();
+  //  Replace _getMessageList
+  Widget _getMessageList(MessageDao messageDao) {
+    return Expanded(
+      child: StreamBuilder<QuerySnapshot>(
+        stream: messageDao.getMessageStream(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: LinearProgressIndicator(),);
+          }
+          return _buildList(context, snapshot.data!.docs);
+        },
+      ),
+    );
   }
 
-  // TODO: Add _buildList
+  // Add _buildList
+  Widget _buildList(BuildContext context, List<DocumentSnapshot>? snapshot) {
+    return ListView(
+      controller: _scrollController,
+      physics: const BouncingScrollPhysics(),
+      padding: const EdgeInsets.only(top: 20.0),
+      children: snapshot!.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
 
-  // TODO: Add _buildListItem
+  // Add _buildListItem
+  Widget _buildListItem(BuildContext context, DocumentSnapshot snapshot) {
+    final message = Message.fromSnapshot(snapshot);
+    return MessageWidget(message.text, message.date, message.email);
+  }
 
   bool _canSendMessage() => _messageController.text.isNotEmpty;
 
