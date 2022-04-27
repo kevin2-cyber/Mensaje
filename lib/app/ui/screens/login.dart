b269c2cc8c7b9dd480cf7d712c612c54c343611f
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-27 14:55:07 +0000

    minor update

--- lib/app/ui/screens/login.dart
+++ lib/app/ui/screens/login.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import '../../../data/dao/user_dao.dart';
 
