643c38b060188120a7edb6b627852b3c70cacf69
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-27 02:45:56 +0000

    created user_dao

---
+++ lib/data/user_dao.dart
@@ -0,0 +1,73 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+
+class UserDao extends ChangeNotifier {
+  final auth = FirebaseAuth.instance;
+
+// Add helper methods
+  bool isLoggedIn() {
+    return auth.currentUser != null;
+  }
+
+  String? userId() {
+    return auth.currentUser?.uid;
+  }
+
+  String? email() {
+    return auth.currentUser?.email;
+  }
+
+// Add signup
+  void signup(String email, String password) async {
+    try {
+      await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      notifyListeners();
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        if (kDebugMode) {
+          print('The password provided is too weak.');
+        }
+      } else if (e.code == 'email-already-in-use') {
+        if (kDebugMode) {
+          print('The account already exists for that email');
+        }
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+  // Add login
+  void login(String email, String password) async {
+    try {
+      await auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      notifyListeners();
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        if (kDebugMode) {
+          print('The password provided is too weak.');
+        }
+      } else if (e.code == 'email-already-in-use') {
+        if (kDebugMode) {
+          print('The account already exists for that email.');
+        }
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+// Add logout
+  void logout() async {
+    await auth.signOut();
+    notifyListeners();
+  }
+}
